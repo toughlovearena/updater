0f7a449c169c740dc57a989c9f4b9a733091a4a7
mpaulweeks <mpaulweeks@gmail.com>
2021-05-26 22:48:59 -0400

    add stop/kill command to bin

--- src/run.ts
+++ src/run.ts
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 // ! is required for bin export aka `npx @toughlovearena/updater`
 
-import { rebuild } from './rebuild';
+import { kill, rebuild } from './rebuild';
 
 if (require.main === module) {
-  // tslint:disable-next-line:no-console
-  console.log('Running @toughlovearena/updater');
-  rebuild();
-
-  // tslint:disable-next-line:no-console
-  console.log(process.argv);
+  const option = process.argv[2];
+  if (['stop', 'kill'].includes(option)) {
+    // tslint:disable-next-line:no-console
+    console.log('Stopping @toughlovearena/updater');
+    kill();
+  } else {
+    // tslint:disable-next-line:no-console
+    console.log('Running @toughlovearena/updater');
+    rebuild();
+  }
 }
