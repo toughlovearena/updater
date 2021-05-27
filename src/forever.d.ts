2ef2bb4b39b3b83cb3bd8f468b439837999fa56d
mpaulweeks <mpaulweeks@gmail.com>
2021-05-27 09:47:34 -0400

    removed unused type declaration

--- src/forever.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'forever' {
-  import * as events from 'events';
-
-  export function startDaemon(target: string): events.EventEmitter;
-  export function stop(target: string): events.EventEmitter;
-}
