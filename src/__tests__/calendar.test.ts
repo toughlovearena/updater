f1cab945616a4d0e999ef96b4b7e12e5b7574399
mpaulweeks <mpaulweeks@gmail.com>
2021-10-12 15:09:51 -0400

    add more to calendar.test

--- src/__tests__/calendar.test.ts
+++ src/__tests__/calendar.test.ts
@@ -10,6 +10,10 @@ describe('Calendar', () => {
   });
 
   test('isInShutdownWindow()', () => {
+    const mondayEvening = '2021-10-11T02:52:16.624Z';
+    timeKeeper._set(new Date(mondayEvening).getTime());
+    expect(sut.isInShutdownWindow()).toBe(false);
+
     const tuesdayAfterMidnight = '2021-10-12T06:52:16.624Z';
     timeKeeper._set(new Date(tuesdayAfterMidnight).getTime());
     expect(sut.isInShutdownWindow()).toBe(true);
@@ -18,12 +22,16 @@ describe('Calendar', () => {
     timeKeeper._set(new Date(tuesdayAfternoon).getTime());
     expect(sut.isInShutdownWindow()).toBe(false);
 
-    const mondayEvening = '2021-10-11T02:52:16.624Z';
-    timeKeeper._set(new Date(mondayEvening).getTime());
-    expect(sut.isInShutdownWindow()).toBe(false);
-
     const wednesdayAfterMidnight = '2021-10-13T06:52:16.624Z';
     timeKeeper._set(new Date(wednesdayAfterMidnight).getTime());
     expect(sut.isInShutdownWindow()).toBe(false);
+
+    const farMondayEvening = '2021-10-25T02:52:16.624Z';
+    timeKeeper._set(new Date(farMondayEvening).getTime());
+    expect(sut.isInShutdownWindow()).toBe(false);
+
+    const farTuesdayAfterMidnight = '2021-10-26T06:52:16.624Z';
+    timeKeeper._set(new Date(farTuesdayAfterMidnight).getTime());
+    expect(sut.isInShutdownWindow()).toBe(true);
   });
 });
